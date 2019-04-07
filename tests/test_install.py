c4d4973520d64f338768b06ded70af15f208b36d
Hunter Senft-Grupp <hunter@schireson.com>
2019-04-07 02:49:02 -0400

    Update simple test

--- tests/test_install.py
+++ tests/test_install.py
@@ -1,12 +1,6 @@
 from sqlalchemy import Table, event
-from sqlalchemy_postgresql_audit.event_listeners.sqlalchemy import create_audit_table
 
 
-def test_install_event_listeners(event_listeners):
+def test_install_event_listeners():
+    from sqlalchemy_postgresql_audit.event_listeners.sqlalchemy import create_audit_table
     assert event.contains(Table, "after_parent_attach", create_audit_table)
-
-
-def test_event_listener_not_installed_on_import():
-    import sqlalchemy_postgresql_audit.event_listeners.sqlalchemy
-
-    assert not event.contains(Table, "after_parent_attach", create_audit_table)
