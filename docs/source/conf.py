9207591b97a6ab4ea732f7ec013b50acb6e4a648
Hunter Senft-Grupp <huntcsg@gmail.com>
2019-06-28 03:14:10 -0400

    Cut Release: 0.3.0 → 0.3.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Hunter Senft-Grupp'
 author = 'Hunter Senft-Grupp'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.3.1'
 
 
 # -- General configuration ---------------------------------------------------
